94b487a81b5fafd832a7be3eedc2513a9499061a
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-06 13:53:31 +0100

    Write gemfile

--- postinstall.js
+++ postinstall.js
@@ -2,6 +2,7 @@ var shell = require('shelljs')
 var semver = require('semver')
 var chalk = require('chalk')
 var path = require('path')
+var fs = require('fs')
 var yes = chalk.green('✓ ')
 var no = chalk.red('✗ ')
 
@@ -37,12 +38,12 @@ var parts = bundlerVersionFull.split(/[\s]+/)
 var bundlerVersion = parts[2]
 var bundlerDir = path.dirname(bundlerPath)
 
-process.stdout.write('==> Checking Ruby ' + config.rubySatisfactory + ' ... ')
+process.stdout.write('==> Checking Ruby \'' + config.rubySatisfactory + '\' ... ')
 if (semver.satisfies(rubyVersion, config.rubySatisfactory)) {
   console.log(yes + rubyVersion + ' (' + rubyVersionFull + ')')
 } else {
   console.log(no + rubyVersion + ' (' + rubyVersionFull + ')')
-  process.stdout.write('--> Checking rvm ' + config.rvmSatisfactory + ' ... ')
+  process.stdout.write('--> Checking rvm \'' + config.rvmSatisfactory + '\' ... ')
   if (semver.satisfies(rvmVersion, config.rvmSatisfactory)) {
     console.log(yes + rvmVersion + ' (' + rvmVersionFull + ')')
   } else {
@@ -52,26 +53,34 @@ if (semver.satisfies(rubyVersion, config.rubySatisfactory)) {
       fatalExe('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3')
       console.log(yes)
     }
-    process.stdout.write('--> Installing rvm ' + config.rvmSatisfactory + ' ... ')
+    process.stdout.write('--> Installing rvm \'' + config.rvmSatisfactory + '\' ... ')
     fatalExe('curl -sSL https://get.rvm.io | bash -s ' + config.rvmDesired)
     console.log(yes)
   }
-  fatalExe(config.rvmCmd + ' install ' + config.rubyDesired + ' && rvm use ' + config.rubySatisfactory + ' && ruby -v')
+  fatalExe(config.rvmCmd + ' install \'' + config.rubyDesired + '\' && rvm use \'' + config.rubySatisfactory + '\' && ruby -v')
 }
 
-process.stdout.write('==> Checking Bundler ' + config.bundlerSatisfactory + ' ... ')
+process.stdout.write('==> Checking Bundler \'' + config.bundlerSatisfactory + '\' ... ')
 if (semver.satisfies(bundlerVersion, config.bundlerSatisfactory)) {
   console.log(yes + bundlerVersion + ' (' + bundlerVersionFull + ')')
 } else {
   console.log(no + bundlerVersion + ' (' + bundlerVersionFull + ')')
   shell.mkdir('-p', bundlerDir)
-  fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + 'gem install bundler -v ' + config.bundlerDesired + ' -n ' + bundlerDir)
+  fatalExe(config.rvmCmd + ' use \'' + config.rubySatisfactory + '\' && ' + 'gem install bundler -v \'' + config.bundlerDesired + '\' -n ' + bundlerDir)
 }
 
 process.stdout.write('==> Configuring Bundler ... ')
-fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' config build.nokogiri --use-system-libraries')
+fatalExe(config.rvmCmd + ' use \'' + config.rubySatisfactory + '\' && ' + bundlerPath + ' config build.nokogiri --use-system-libraries')
 console.log(yes)
 
 process.stdout.write('==> Installing Gems ... ')
-fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' install --path ' + __dirname + '/deps/gems || ' + 'rvm use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' update')
+
+var buf = 'source \'https://rubygems.org\'\n'
+for (var name in config.gems) {
+  var version = config.gems[name]
+  buf += 'gem \'' + name + '\', \'' + version + '\'\n'
+}
+fs.writeFileSync(__dirname + '/Gemfile', buf, 'utf-8')
+
+fatalExe(config.rvmCmd + ' use \'' + config.rubySatisfactory + '\' && ' + bundlerPath + ' install --path ' + __dirname + '/deps/gems || ' + 'rvm use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' update')
 console.log(yes)
