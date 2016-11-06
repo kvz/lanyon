099130dc266c42c3c78d87498985b4c67a55a862
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-06 13:48:51 +0100

    Also check rvm

--- postinstall.js
+++ postinstall.js
@@ -5,7 +5,6 @@ var path = require('path')
 var yes = chalk.green('✓ ')
 var no = chalk.red('✗ ')
 
-
 function fatalExe (cmd) {
   var opts = { 'silent': true }
   var p = shell.exec(cmd, opts)
@@ -20,49 +19,59 @@ function fatalExe (cmd) {
   return p.stdout.trim()
 }
 
-var desiredRuby = '2.1.1'
-var satisfactoryRuby = '>=2'
-var desiredBundler = '1.13.0'
-var satisfactoryBundler = '>=1'
+var config = require('./package.json').lanyon
 
 var rubyPath = shell.which('ruby')
-var rubyVersionFull = fatalExe(rubyPath + ' -v')
-var rubyVersion = rubyVersionFull.split(/[p\s]+/)[1]
+var rubyVersionFull = shell.exec(rubyPath + ' -v', { 'silent': true }).stdout.trim()
+var parts = rubyVersionFull.split(/[p\s]+/)
+var rubyVersion = parts[1]
+
 var rvmPath = shell.which('rvm')
+var rvmVersionFull = shell.exec(rvmPath + ' -v', { 'silent': true }).stdout.trim()
+var parts = rvmVersionFull.split(/[p\s]+/)
+var rvmVersion = parts[1]
 
 var bundlerPath = __dirname + '/deps/bin/bundler'
-var bundlerVersionFull = fatalExe(bundlerPath + ' -v')
-var bundlerVersion = bundlerVersionFull.split(/\s+/).pop()
+var bundlerVersionFull = shell.exec(bundlerPath + ' -v', { 'silent': true }).stdout.trim()
+var parts = bundlerVersionFull.split(/[\s]+/)
+var bundlerVersion = parts[2]
 var bundlerDir = path.dirname(bundlerPath)
 
-
-process.stdout.write('--> Checking Ruby ' + satisfactoryRuby + ' ... ');
-if (semver.satisfies(rubyVersion, satisfactoryRuby)) {
+process.stdout.write('==> Checking Ruby ' + config.rubySatisfactory + ' ... ')
+if (semver.satisfies(rubyVersion, config.rubySatisfactory)) {
   console.log(yes + rubyVersion + ' (' + rubyVersionFull + ')')
 } else {
   console.log(no + rubyVersion + ' (' + rubyVersionFull + ')')
-  if (!rvmPath) {
+  process.stdout.write('--> Checking rvm ' + config.rvmSatisfactory + ' ... ')
+  if (semver.satisfies(rvmVersion, config.rvmSatisfactory)) {
+    console.log(yes + rvmVersion + ' (' + rvmVersionFull + ')')
+  } else {
+    console.log(no + rvmVersion + ' (' + rvmVersionFull + ')')
     if (shell.test('-f', '/etc/apt/sources.list').code === 0) {
+      process.stdout.write('--> Setting up GPG for rvm ' + config.rvmSatisfactory + ' ... ')
       fatalExe('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3')
+      console.log(yes)
     }
-    fatalExe('curl -sSL https://get.rvm.io | bash -s stable')
+    process.stdout.write('--> Installing rvm ' + config.rvmSatisfactory + ' ... ')
+    fatalExe('curl -sSL https://get.rvm.io | bash -s ' + config.rvmDesired)
+    console.log(yes)
   }
-  fatalExe('type rvm | head -n 1 && rvm install ' + desiredRuby + ' && rvm use ' + satisfactoryRuby + ' && ruby -v')
+  fatalExe(config.rvmCmd + ' install ' + config.rubyDesired + ' && rvm use ' + config.rubySatisfactory + ' && ruby -v')
 }
 
-process.stdout.write('--> Checking Bundler ' + satisfactoryBundler + ' ... ');
-if (semver.satisfies(bundlerVersion, satisfactoryBundler)) {
+process.stdout.write('==> Checking Bundler ' + config.bundlerSatisfactory + ' ... ')
+if (semver.satisfies(bundlerVersion, config.bundlerSatisfactory)) {
   console.log(yes + bundlerVersion + ' (' + bundlerVersionFull + ')')
 } else {
   console.log(no + bundlerVersion + ' (' + bundlerVersionFull + ')')
   shell.mkdir('-p', bundlerDir)
-  fatalExe('gem install bundler -v ' + desiredBundler + ' -n ' + bundlerDir)
+  fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + 'gem install bundler -v ' + config.bundlerDesired + ' -n ' + bundlerDir)
 }
 
-process.stdout.write('--> Configuring Bundler ... ')
-fatalExe(bundlerPath + ' config build.nokogiri --use-system-libraries')
+process.stdout.write('==> Configuring Bundler ... ')
+fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' config build.nokogiri --use-system-libraries')
 console.log(yes)
 
-process.stdout.write('--> Installing Gems ... ')
-fatalExe(bundlerPath + ' install --path ' + __dirname + '/deps/gems || ' + bundlerPath + ' update')
+process.stdout.write('==> Installing Gems ... ')
+fatalExe(config.rvmCmd + ' use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' install --path ' + __dirname + '/deps/gems || ' + 'rvm use ' + config.rubySatisfactory + ' && ' + bundlerPath + ' update')
 console.log(yes)
