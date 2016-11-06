14608dc81c78e6ee5412ffdf3dd290e0db1c7e7f
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-06 13:14:09 +0100

    Prettier output

--- postinstall.js
+++ postinstall.js
@@ -1,42 +1,68 @@
 var shell = require('shelljs')
 var semver = require('semver')
+var chalk = require('chalk')
+var path = require('path')
+var yes = chalk.green('✓ ')
+var no = chalk.red('✗ ')
 
+
+function fatalExe (cmd) {
+  var opts = { 'silent': true }
+  var p = shell.exec(cmd, opts)
+
+  if (p.code !== 0) {
+    console.log(no)
+    console.error(p.stdout)
+    console.error(p.stderr)
+    shell.exit(1)
+  }
+
+  return p.stdout.trim()
+}
+
+var desiredRuby = '2.1.1'
+var satisfactoryRuby = '>=2'
+var desiredBundler = '1.13.0'
+var satisfactoryBundler = '>=1'
 
 var rubyPath = shell.which('ruby')
-var rubyVersion = shell.exec(rubyPath + '-v').stdout
+var rubyVersionFull = fatalExe(rubyPath + ' -v')
+var rubyVersion = rubyVersionFull.split(/[p\s]+/)[1]
 var rvmPath = shell.which('rvm')
 
-console.log('Installing ruby...')
-if (!rubyPath || !semver.satisfies(rubyVersion, '>=2')) {
+var bundlerPath = __dirname + '/deps/bin/bundler'
+var bundlerVersionFull = fatalExe(bundlerPath + ' -v')
+var bundlerVersion = bundlerVersionFull.split(/\s+/).pop()
+var bundlerDir = path.dirname(bundlerPath)
+
+
+process.stdout.write('--> Checking Ruby ' + satisfactoryRuby + ' ... ');
+if (semver.satisfies(rubyVersion, satisfactoryRuby)) {
+  console.log(yes + rubyVersion + ' (' + rubyVersionFull + ')')
+} else {
+  console.log(no + rubyVersion + ' (' + rubyVersionFull + ')')
   if (!rvmPath) {
-    if (shell.test('-f', '/etc/apt/sources.list').code !== 0) {
-      if (shell.exec('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 ').code !== 0) {
-        shell.exit(1)
-      }
+    if (shell.test('-f', '/etc/apt/sources.list').code === 0) {
+      fatalExe('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3')
     }
-    if (shell.exec('curl -sSL https://get.rvm.io | bash -s stable').code !== 0) {
-      shell.exit(1)
-    }
+    fatalExe('curl -sSL https://get.rvm.io | bash -s stable')
   }
-  if (shell.exec('type rvm | head -n 1 && rvm install 2.1.1 && rvm use 2.1.1 && ruby -v').code !== 0) {
-    shell.exit(1)
-  }
+  fatalExe('type rvm | head -n 1 && rvm install ' + desiredRuby + ' && rvm use ' + satisfactoryRuby + ' && ruby -v')
 }
 
-console.log('Installing bundler...')
-if (!shell.test('-f', __dirname + '/deps/bin/bundle')) {
-  shell.mkdir('-p', __dirname + '/deps/bin')
-  if (shell.exec('gem install bundler -v 1.13.0 -n ' + __dirname + '/deps/bin').code !== 0) {
-    shell.exit(1)
-  }
+process.stdout.write('--> Checking Bundler ' + satisfactoryBundler + ' ... ');
+if (semver.satisfies(bundlerVersion, satisfactoryBundler)) {
+  console.log(yes + bundlerVersion + ' (' + bundlerVersionFull + ')')
+} else {
+  console.log(no + bundlerVersion + ' (' + bundlerVersionFull + ')')
+  shell.mkdir('-p', bundlerDir)
+  fatalExe('gem install bundler -v ' + desiredBundler + ' -n ' + bundlerDir)
 }
 
-console.log('Configuring bundler...')
-if (shell.exec(__dirname + '/deps/bin/bundle config build.nokogiri --use-system-libraries').code !== 0) {
-  shell.exit(1)
-}
+process.stdout.write('--> Configuring Bundler ... ')
+fatalExe(bundlerPath + ' config build.nokogiri --use-system-libraries')
+console.log(yes)
 
-console.log('Installing gems...')
-if (shell.exec(__dirname + '/deps/bin/bundle install --path ' + __dirname + '/deps/gems || bundle update').code !== 0) {
-  shell.exit(1)
-}
+process.stdout.write('--> Installing Gems ... ')
+fatalExe(bundlerPath + ' install --path ' + __dirname + '/deps/gems || ' + bundlerPath + ' update')
+console.log(yes)
