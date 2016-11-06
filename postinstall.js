c86aace8298ef38b8b10af78f72b07aaddac1a84
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-06 12:48:50 +0100

    Only execute gpg on ubuntu

--- postinstall.js
+++ postinstall.js
@@ -9,7 +9,12 @@ var rvmPath = shell.which('rvm')
 console.log('Installing ruby...')
 if (!rubyPath || !semver.satisfies(rubyVersion, '>=2')) {
   if (!rvmPath) {
-    if (shell.exec('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 && curl -sSL https://get.rvm.io | bash -s stable').code !== 0) {
+    if (shell.test('-f', '/etc/apt/sources.list').code !== 0) {
+      if (shell.exec('gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 ').code !== 0) {
+        shell.exit(1)
+      }
+    }
+    if (shell.exec('curl -sSL https://get.rvm.io | bash -s stable').code !== 0) {
       shell.exit(1)
     }
   }
