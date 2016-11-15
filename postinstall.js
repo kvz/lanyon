8b7e52cb7765a020140d474e0629872a6b3cd775
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-15 11:10:01 +0100

    Docker fixes

--- postinstall.js
+++ postinstall.js
@@ -81,9 +81,9 @@ if (!satisfied('node')) {
 }
 
 if (satisfied('docker')) {
-  rubyExe = 'docker run -v $PWD:/srv/jekyll jekyll/jekyll ruby'
-  // jekyllExe = 'docker run -v $PWD:/srv/jekyll -p "' + mergedCfg.ports.content + ':4000" jekyll/jekyll jekyll'
-  jekyllExe = 'docker run --rm -it -p ' + mergedCfg.ports.content + ':4000 -v $PWD:/site madduci/docker-github-pages'
+  // rubyExe = 'docker run -v $PWD:/srv/jekyll jekyll/jekyll ruby'
+  jekyllExe = 'docker run --interactive --tty --volume $PWD:/srv/jekyll --publish "' + mergedCfg.ports.content + ':4000" jekyll/jekyll bundler update; bundler config build.nokogiri --use-system-libraries; bundler install --path /srv/deps; bundler update; bundler exec jekyll'
+  // jekyllExe = 'docker run --rm -it -p ' + mergedCfg.ports.content + ':4000 -v $PWD:/site madduci/docker-github-pages'
 } else {
   if (!satisfied('ruby')) {
     if (satisfied('rbenv')) {
