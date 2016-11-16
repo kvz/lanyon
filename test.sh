b0e1c22fd3e4306220985204cc020f493c3297da
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-11-16 20:43:06 +0100

    Easier debugging on Travis

--- test.sh
+++ test.sh
@@ -7,18 +7,18 @@ set -o nounset
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 if type md5sum 2>/dev/null; then
-  mdfive=md5sum
+  cmdMd5=md5sum
 elif type md5 2>/dev/null; then
-  mdfive=md5
+  cmdMd5=md5
 else
   echo "No md5 program found"
   exit 1
 fi
 
 if type yarn 2>/dev/null; then
-  enpeeem=yarn
+  cmdNpm=yarn
 elif type npm 2>/dev/null; then
-  enpeeem=npm
+  cmdNpm=npm
 else
   echo "No npm program found"
   exit 1
@@ -26,7 +26,7 @@ fi
 
 echo "${__dir}"
 
-${enpeeem} link || true
+${cmdNpm} link || true
 
 # Cross-platform mktemp: http://unix.stackexchange.com/questions/30091/fix-or-alternative-for-mktemp-in-os-x
 tdir=$(mktemp -d 2>/dev/null || mktemp -d -t 'lanyon')
@@ -52,10 +52,13 @@ title: home
 ---
 EOF
 
-  ${enpeeem} link lanyon
+  ${cmdNpm} link lanyon
 
-  PROJECT_DIR=$(pwd) npm explore lanyon -- ${enpeeem} run build
+  set -x
+  PROJECT_DIR=$(pwd) npm explore lanyon -- ${cmdNpm} run build
+  cat node_modules/lanyon/vendor/bin/jekyll
   find .
-  ${mdfive} ./_site/index.html |tee |grep 68b329da9893e34099c7d8ad5cb9c940
+  ${cmdMd5} ./_site/index.html |tee |grep 68b329da9893e34099c7d8ad5cb9c940
+  set +x
 popd
 rm -rf "${tdir}"
