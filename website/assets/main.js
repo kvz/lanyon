358b00e2290bf02ecb9c37cf16f2361e3c133b22
Kevin van Zonneveld <kevin@vanzonneveld.net>
2016-12-16 14:47:53 +0100

    Use text-logo for now

--- website/assets/main.js
+++ website/assets/main.js
@@ -1,43 +1,23 @@
 // Backgrounds.
 // var backgrounds = [1,2,3,4,5,6]
-var backgrounds = [1]
-
 // Highlighting.
-hljs.initHighlighting()
-
-// Storage.
-var store = window.sessionStorage
-
-// Background.
-var background = store.getItem('background')
-
-if (!background) {
-  var i = Math.random() * backgrounds.length | 0
-  background = backgrounds[i]
-  console.log('setting background to %s', background)
-  store.setItem('background', background)
-}
-
-// Body class for background.
-var el = document.getElementById('header-overlay')
-el.style.backgroundImage = 'url(/assets/images/' + background + '.jpg)'
+hljs.initHighlighting() // eslint-disable-line no-undef
 
 // Attach class to #menu element depending on page offset.
 document.addEventListener('DOMContentLoaded', chooseMenuColor)
 window.onscroll = chooseMenuColor
 
-function chooseMenuColor() {
+function chooseMenuColor () {
   var menuElement = document.getElementById('menu')
   var menuTopOffset = 45
-  Math.floor(window.innerHeight * .20) < window.pageYOffset + menuTopOffset ?
-    menuElement.classList.remove('over-header') :
-    menuElement.classList.add('over-header')
+  Math.floor(window.innerHeight * 0.20) < window.pageYOffset + menuTopOffset
+    ? menuElement.classList.remove('over-header')
+    : menuElement.classList.add('over-header')
 }
 
-
-function $get(selector) { return document.querySelector(selector) }
-function $all(selector) {
-	  return Array.prototype.slice.call(document.querySelectorAll(selector))
+function $get (selector) { return document.querySelector(selector) }
+function $all (selector) {
+  return Array.prototype.slice.call(document.querySelectorAll(selector))
 }
 
 var container = $get('#menu-items')
@@ -48,8 +28,8 @@ var container = $get('#menu-items')
 //   container.appendChild(li);
 // }
 
-$all('#content h2').forEach(function(el) {
-  var li = document.createElement('li');
-  li.innerHTML = '<a href="#' + el.id + '">' + el.innerHTML + '</a>';
-  container.appendChild(li);
+$all('#content h2').forEach(function (el) {
+  var li = document.createElement('li')
+  li.innerHTML = '<a href="#' + el.id + '">' + el.innerHTML + '</a>'
+  container.appendChild(li)
 })
