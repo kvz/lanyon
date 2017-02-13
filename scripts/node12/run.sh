52446e28a37c95d892c20d7f9b3d0c52208f3bf3
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-02-13 13:00:04 +0100

    Add Dockerfile for testing building on node 0.12 locally

---
+++ scripts/node12/run.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+set -o pipefail
+set -o errexit
+set -o nounset
+set -o xtrace
+
+__dirname="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__scriptsdir="$(cd "$(dirname "${__dirname}")" && pwd)"
+__rootdir="$(cd "$(dirname "${__scriptsdir}")" && pwd)"
+
+pushd "${__dirname}"
+  docker build . \
+    --no-cache \
+    --tag lanyon-base:0.12
+
+  # --rm \
+  docker run \
+    -it \
+    --volume "${__rootdir}:/app" \
+    lanyon-base:0.12 \
+    sh -ci " \
+      npm install && npm run build
+    "
+popd
