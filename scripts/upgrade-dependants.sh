1dd2ec5b49737137d3c87e2562b615267519c20c
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-02-15 18:42:10 +0100

    Fix dependants

--- scripts/upgrade-dependants.sh
+++ scripts/upgrade-dependants.sh
@@ -15,13 +15,18 @@ version=$(node -e 'console.log(require("./package.json").version)')
 for dir in ~/code/content ~/code/kvz.io ~/code/legal ~/code/lanyon/example ~/code/lanyon/website ~/code/transloadify ~/code/tus.io ~/code/frey-website ~/code/bash3boilerplate; do
   pushd ${dir}
     npm unlink lanyon || true
-    gsed -i .travis.yml -e 's/lanyon postinstall/lanyon install/g' || true
-    gsed -i package.json -e 's/lanyon postinstall/lanyon install/g' || true
-    gsed -i website/package.json -e 's/lanyon postinstall/lanyon install/g' || true
+
     gsed -i _scripts/postinstall.sh -e 's/lanyon postinstall/lanyon install/g' || true
+    git add _scripts/postinstall.sh || true
+    gsed -i .travis.yml -e 's/lanyon postinstall/lanyon install/g' || true
+    git add .travis.yml || true
+    gsed -i package.json -e 's/lanyon postinstall/lanyon install/g' || true
+    git add package.json || true
+
     yarn add lanyon@${version}
+    git add yarn.lock || true
     env LANYON_RESET=1 node "${__dir}/../lib/cli.js" install
-    git add package.json yarn.lock _scripts/postinstall.sh website/package.json .travis.yml || true
+
     git commit -m "Upgrade Lanyon to v${version}" || true
     git pull && git push
   popd
