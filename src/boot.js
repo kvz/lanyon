033519e7ffbd89a876f5889cf72ac6a6ed1335bf
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-03-23 14:13:57 +0100

    Pass down `DEBUG` env var

--- src/boot.js
+++ src/boot.js
@@ -38,6 +38,7 @@ module.exports = async function boot (whichPackage) {
     addCommandAsComponent: true,
     components           : `lanyon>${cmdName}`,
     env                  : Object.assign({}, process.env, {
+      DEBUG         : process.env.DEBUG,
       NODE_ENV      : runtime.lanyonEnv,
       JEKYLL_ENV    : runtime.lanyonEnv,
       LANYON_PROJECT: runtime.projectDir, // <-- to preserve the cwd over multiple nested executes, if it wasn't initially set
