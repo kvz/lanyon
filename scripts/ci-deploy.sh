647cfcdc9f64b204d5407cf199e8ccec32e03434
Kevin van Zonneveld <kevin@vanzonneveld.net>
2017-04-12 11:02:02 +0200

    Fix deploys

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -1,17 +1,39 @@
 #!/bin/sh -ex
+echo "--> LANYON_USE_LINKED=${LANYON_USE_LINKED:-}"
+
 if egrep '"lanyon": "\d+\.\d+\.\d+"' package.json; then
   cd .
-fi
-if egrep '"lanyon": "\d+\.\d+\.\d+"' website/package.json; then
+  echo " --> in $(pwd)"
+elif egrep '"lanyon": "\d+\.\d+\.\d+"' website/package.json; then
   cd website
+  echo " --> in $(pwd)"
+else
+  echo "==> unable to locate lanyon dependency in package.json or website/package.json"
+  exit 1
 fi
 
 if [ ! -f node_modules/.bin/lanyon ]; then
-  yarn || npm install
+  if [ "${LANYON_USE_LINKED:-}" = "1" ]; then
+    npm link lanyon
+  else
+    yarn || npm install
+  fi
 fi
 
-node_modules/.bin/lanyon install
+if [ ! -f node_modules/.bin/lanyon ]; then
+  if [ -f lib/cli.js ]; then
+    node lib/cli.js install
+  elif [ -f ../lib/cli.js ]; then
+    node ../lib/cli.js install
+  else
+    echo "==> unable to locate lanyon cli in node_modules/.bin/lanyon or lib/cli.js or ../lib/cli.js"
+    exit 1
+  fi
+else
+  node_modules/.bin/lanyon install
+fi
 
 (npm run build:production || npm run web:build:production) && (npm run deploy || npm run web:deploy)
 
 cd -
+echo " --> in $(pwd)"
