e088e12ec53e357e28319c7da55ac5be14781faa
Kevin van Zonneveld <kevin@vanzonneveld.net>
2022-07-25 21:41:24 +0200

    One more try

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,22 +13,22 @@ module.exports = {
   rules: {
     // useless rules for this project
     /// /////////////////////////////////////////////////////////
-    'no-console'                : ['off'],
+    'no-console'                       : ['off'],
     // rules we had to turn off just to get a pass, but we'd
     // like to turn on one by one with separate PRs
     /// /////////////////////////////////////////////////////////
-    'consistent-return'         : ['warn'],
-    'global-require'            : ['warn'],
-    'guard-for-in'              : ['warn'],
-    'import/no-dynamic-require' : ['warn'],
-    'import/order'              : ['warn'],
-    'import/extensions'         : ['warn'],
-    'no-param-reassign'         : ['warn'],
-    'no-restricted-syntax'      : ['warn'],
-    'no-shadow'                 : ['warn'],
-    'no-unused-expressions'     : ['warn'],
-    'no-use-before-define'      : ['warn'],
-    'no-named-as-default-member': ['warn'],
+    'consistent-return'                : ['warn'],
+    'global-require'                   : ['warn'],
+    'guard-for-in'                     : ['warn'],
+    'import/no-dynamic-require'        : ['warn'],
+    'import/order'                     : ['warn'],
+    'import/extensions'                : ['warn'],
+    'no-param-reassign'                : ['warn'],
+    'no-restricted-syntax'             : ['warn'],
+    'no-shadow'                        : ['warn'],
+    'no-unused-expressions'            : ['warn'],
+    'no-use-before-define'             : ['warn'],
+    'import/no-named-as-default-member': ['warn'],
   },
   overrides: [
     {
