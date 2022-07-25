dc6b0f9d28f1b148673feee388c9547c64f01170
Kevin van Zonneveld <kevin@vanzonneveld.net>
2022-07-25 21:39:53 +0200

    Turn no-named-as-default-member into a warning in preparation of more typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,21 +13,22 @@ module.exports = {
   rules: {
     // useless rules for this project
     /// /////////////////////////////////////////////////////////
-    'no-console'               : ['off'],
+    'no-console'                : ['off'],
     // rules we had to turn off just to get a pass, but we'd
     // like to turn on one by one with separate PRs
     /// /////////////////////////////////////////////////////////
-    'consistent-return'        : ['warn'],
-    'global-require'           : ['warn'],
-    'guard-for-in'             : ['warn'],
-    'import/no-dynamic-require': ['warn'],
-    'import/order'             : ['warn'],
-    'import/extensions'        : ['warn'],
-    'no-param-reassign'        : ['warn'],
-    'no-restricted-syntax'     : ['warn'],
-    'no-shadow'                : ['warn'],
-    'no-unused-expressions'    : ['warn'],
-    'no-use-before-define'     : ['warn'],
+    'consistent-return'         : ['warn'],
+    'global-require'            : ['warn'],
+    'guard-for-in'              : ['warn'],
+    'import/no-dynamic-require' : ['warn'],
+    'import/order'              : ['warn'],
+    'import/extensions'         : ['warn'],
+    'no-param-reassign'         : ['warn'],
+    'no-restricted-syntax'      : ['warn'],
+    'no-shadow'                 : ['warn'],
+    'no-unused-expressions'     : ['warn'],
+    'no-use-before-define'      : ['warn'],
+    'no-named-as-default-member': ['warn'],
   },
   overrides: [
     {
